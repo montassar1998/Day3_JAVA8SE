43a3dd0857e0a7fae48a128429cffb424d659c8b
montassar1998 <montassarriahi@aol.com>
2022-03-30 12:42:14 +0200

    control structures and iterative structures while for

--- src/Day3.java
+++ src/Day3.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+import java.util.Map;
 
 public class Day3 {
 
@@ -64,6 +66,60 @@ public class Day3 {
             case s:
             case "ab":System.out.println("S is equal to AB");break;
             default : System.out.println("Default");break;
+        }
+        // loops
+        int counter=5;
+        // the condition below is never satisfied thus, infinite loop
+        /*while(counter>0){
+            System.out.println("5");
+        }*/
+
+        // the condition below is always false, thus there is no iteration of the while loop
+
+        while(counter<0){
+            System.out.println("5");
+        }
+
+        // do while
+        //even though the condition 5<0 is false, the loop executes one iteration and then jumps to the next bloc of code
+        // do while ensures at least one execution
+        do{
+            System.out.println("5");
+        }while(counter<0);
+
+
+        // for loops
+
+        for(int c=0;c<10;c++) {
+            System.out.println("c = "+c);
+            //c = 0
+            //c = 1
+            //c = 2
+            //c = 3
+            //c = 4
+            //c = 5
+            //c = 6
+            //c = 7
+            //c = 8
+            //c = 9
+        }
+        //for loop to traverse a table
+        int[] tab= {37,83,49};
+        int c=0;
+        for(;c<tab.length;){
+            System.out.println("t["+c+"] = "+tab[c]);
+            c++;
+        }
+        OUTER_LOOP: for(int k=0;k<tab.length;k++){
+            INNER_LOOP:for(int inner=0;inner<tab.length;inner++){
+                System.out.println(" k = "+ k + " x = "+inner);
+            }
+            //this code display the elements k and l (the values)
+
+        }
+        //each loop
+        for(int a:tab){
+            System.out.println(a);
         }
 
     }
