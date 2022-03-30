5d3243a636ed44c9cd535322b1b5a4d9dbddcdb2
montassar1998 <montassarriahi@aol.com>
2022-03-30 10:54:29 +0200

    Hello

--- src/Day3.java
+++ src/Day3.java
@@ -2,7 +2,14 @@ public class Day3 {
     public static void main(String[] args) {
         int i=1;
         int j = i++ + i++;
-        j++;
-        System.out.println(j);
+        long l = i;
+        i = (int) l;
+        System.out.println(i);
+        i = (int) 2.5;
+        System.out.println(i);
+        byte b= (byte) 127;
+        // short is coded on 16 bits
+        // It has a minimum value of -32,768 and a maximum value of 32,767 (inclusive).
+        System.out.println(b);
     }
 }
